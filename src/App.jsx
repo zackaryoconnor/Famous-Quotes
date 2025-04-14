205e2f108384ae68d9b769d24b05369e26aff208
Conor Wantuch <cwantuch7@gmail.com>
2025-04-14 12:17:07 -0600

    set up adding a quote, changed navbar routes and App.jsx to have only one Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import './App.css'
-import Navbar from './components/Navbar.jsx'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import CreateQuote from './components/CreateQuote';
+import Home from './components/Home';
 
 
 function App() {
 
   return (
-   <>
+    <Router>
       <Navbar />
-   </>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/addQuote" element={<CreateQuote />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
