07d526a19a0b0e0b280cd663de03535e6f61fd73
Conor Wantuch <cwantuch7@gmail.com>
2025-04-15 10:23:25 -0600

    adding a random quote to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,25 @@
-import React from 'react';
-const Home = () => <h1>Welcome to the Home Page</h1>;
+import React, { useEffect, useState } from "react"
+
+const Home = () => {
+const [quotes, setQuotes] = useState([]);
+const [randomQuote, setRandomQuote] = useState(null)
+
+   useEffect(() => {
+    const  getQuotes = async () => {
+        const response = await fetch(`http://localhost:3000/quotes`);
+        const data = await response.json();
+        setQuotes(data)
+        if (data.length > 0) {
+            const randomIndex = Math.floor(Math.random() * data.length);
+            setRandomQuote(data[randomIndex])
+        }
+    }
+    getQuotes();
+   }, [])
+
+    return(
+        <h1>HomePage </h1>
+    )
+    
+}
 export default Home;
