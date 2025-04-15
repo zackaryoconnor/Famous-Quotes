35280bf6d2585ec68de34c911ad31794b95f6c1a
Zack OConnor <zackary25@gmail.com>
2025-04-15 15:21:11 -0400

    Added a filter button

--- src/components/Quotes/Quotes.jsx
+++ src/components/Quotes/Quotes.jsx
@@ -1,41 +1,56 @@
-import React, { useEffect, useState } from "react"
-import QuotesCard from "./QuotesCard"
+import React, { useEffect, useState } from "react";
+import QuotesCard from "./QuotesCard";
 
 const Quotes = () => {
-
-    const [quotes, setQuotes] = useState([])
-
+    const [quotes, setQuotes] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState("All Categories");
 
     const fetchData = async () => {
-        const url = `http://localhost:3000/quotes`
+        const url = `http://localhost:3000/quotes`;
 
         try {
-            const response = await fetch(url)
-            const data = await response.json()
-            setQuotes(data)
+            const response = await fetch(url);
+            const data = await response.json();
+            setQuotes(data);
         } catch (error) {
-            console.error(`Error fectching data`, error)
+            console.error(`Error fetching data`, error);
         }
-    }
-
-
-
+    };
 
     useEffect(() => {
-        fetchData()
-    }, [])
+        fetchData();
+    }, []);
 
+    const filteredQuotes =
+        selectedCategory === "All Categories"
+            ? quotes
+            : quotes.filter((quote) => quote.category === selectedCategory);
 
-
+    const categories = ["All Categories", ...new Set(quotes.map((quote) => quote.category))];
 
     return (
-        <div className="mx-24 mt-32 grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-16">
-            { quotes.map((quote) => (
-                <QuotesCard key={ quote._id } quote={ quote } />
-                
-            ))}
-        </div>
-    )
-}
+        <div>
+            <div className="mt-8 mx-24">
+            <select
+                value={selectedCategory}
+                onChange={(event) => setSelectedCategory(event.target.value)}
+                className="mb-4 p-2 border rounded"
+            >
+                {categories.map((category) => (
+                    <option key={category} value={category}>
+                        {category}
+                    </option>
+                ))}
+            </select>
+            </div>
 
-export default Quotes
+            <div className="mx-24 mt-32 grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-16">
+                {filteredQuotes.map((quote) => (
+                    <QuotesCard key={quote._id} quote={quote} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Quotes;
